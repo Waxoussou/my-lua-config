78d693f63c864a15def65c9ac7020b16489be9d7
maxime.abehserra <maxime.abehserra@bluemind.net>
2024-12-15 00:00:26 +0100

    Plugin: lualine - use opts instead of config in plugin configuration

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,11 +1,9 @@
 return {
   'nvim-lualine/lualine.nvim',
-  config = function()
-    require('lualine').setup({
-      options = {
-        theme = 'dracula'
-      }
-    })
-  end
+  opts = {
+    options = {
+      theme = 'dracula'
+    },
+  },
 }
 
