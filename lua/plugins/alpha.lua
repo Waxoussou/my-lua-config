16443475a6e61726ffedc192f94d22a5bd710b68
maxime.abehserra <maxime.abehserra@bluemind.net>
2024-12-17 13:54:56 +0100

    Plugins: Alpha - add ascii logo of Bluemind

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,7 +1,46 @@
 return {
-    'goolord/alpha-nvim',
-    dependencies = { 'echasnovski/mini.icons' },
-    config = function ()
-        require'alpha'.setup(require'alpha.themes.dashboard'.config)
-    end
+  'goolord/alpha-nvim',
+  dependencies = { 'echasnovski/mini.icons' },
+  opts = function ()
+    local dashboard = require("alpha.themes.dashboard")
+
+    local logo = [[
+                                           
+                   :::::                   
+                :::::::::::                
+             :::::::::::::::::             
+          ::::::           ::::::          
+         :::::     +++++     :::::         
+         ::::   +++++++++++   ::::         
+         ::::-+++++++++++++++-::::         
+         :==++++++++   ++++++++==:         
+      :::-+++++++         +++++++-:::      
+   ::::::-++++               ++++-::::::   
+   ::::::=++++               ++++=::::::   
+   ::::: +++++               +++++ :::::   
+   ::::: +++++               +++++ :::::   
+   ::::: +++++               +++++ :::::   
+   ::::: +++++++++       +++++++++ :::::   
+   :::::  +++++++++++ +++++++++++  :::::   
+   ::::::    +++++++++++++++++    ::::::   
+    :::::::     ===+++++===     :::::::    
+       :::::::::::::-=-:::::::::::::       
+          ::::::::::: :::::::::::          
+             ::::         ::::             
+
+    ]]
+
+--    local function pick_color()
+--        local colors = {"String", "Identifier", "Keyword", "Number"}
+--        return colors[math.random(#colors)]
+--    end
+    dashboard.section.header.val = vim.split(logo, "\n")
+    dashboard.section.header.opts.hl = "Identifier"
+--    dashboard.section.header.opts = { hl = { {"AlphaHeader",13,20 }} }
+    return dashboard
+  end,
+
+  config = function ()
+      require'alpha'.setup(require'alpha.themes.dashboard'.config)
+  end
 }
