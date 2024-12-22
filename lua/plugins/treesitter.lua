6a59ae14055d9a8e3f338bbe59a72d54d1582ae5
maxime.abehserra <maxime.abehserra@bluemind.net>
2024-12-22 23:49:50 +0100

    Plugins: treesitter - indent of treesitter not stable , set back to false

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,12 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    local configs = require("nvim-treesitter.configs")
-    configs.setup({
-      ensure_installed = { "lua", "javascript", "typescript", "vue", "java" },
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
+		configs.setup({
+			ensure_installed = { "lua", "javascript", "typescript", "vue", "java" },
+			highlight = { enable = true },
+			indent = { enable = false },
+		})
+	end,
 }
