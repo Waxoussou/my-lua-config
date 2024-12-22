5b52db85bc76e9e0891c614f5bcae7f391277432
maxime.abehserra <maxime.abehserra@bluemind.net>
2024-12-22 18:37:18 +0100

    Plugin: treesitter - add java to config ensure_installed

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     local configs = require("nvim-treesitter.configs")
     configs.setup({
-      ensure_installed = { "lua", "javascript", "typescript", "vue" },
+      ensure_installed = { "lua", "javascript", "typescript", "vue", "java" },
       highlight = { enable = true },
       indent = { enable = true },
     })
