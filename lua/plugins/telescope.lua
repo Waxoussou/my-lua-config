6925de872e825e500b20627f7e1f7266fbc8f5f9
maxime.abehserra <maxime.abehserra@bluemind.net>
2024-12-22 22:33:09 +0100

    Plugins: Telescope - layout setting for some pickers and onCursor for ui-select

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,7 +3,6 @@ return {
 		"nvim-telescope/telescope.nvim",
 		tag = "0.1.8",
 		dependencies = { "nvim-lua/plenary.nvim" },
-
 		config = function()
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<C-p>", builtin.find_files, {})
@@ -13,34 +12,39 @@ return {
 	{
 		"nvim-telescope/telescope-ui-select.nvim",
 		config = function()
+			local layout = {
+				results_title = false,
+				sorting_strategy = "ascending",
+				layout_strategy = "center",
+				layout_config = {
+					preview_cutoff = 1, -- Preview should always show (unless previewer = false)
+					anchor = "N",
+					prompt_position = "top",
+					mirror = true,
+					width = function(_, max_columns, _)
+						return math.min(max_columns, 120)
+					end,
+					height = function(_, _, max_lines)
+						return math.min(max_lines, 10)
+					end,
+				},
+				border = true,
+				borderchars = {
+					prompt = { "─", "│", " ", "│", "╭", "╮", "│", "│" },
+					results = { "─", "│", "─", "│", "├", "┤", "╯", "╰" },
+					preview = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+				},
+			}
 			require("telescope").setup({
 				--				defaults = { path_display = { "tail" } },
-				defaults = {
-					results_title = false,
-					--					sorting_strategy = "ascending",
-					layout_strategy = "center",
-					layout_config = {
-						preview_cutoff = 1, -- Preview should always show (unless previewer = false)
-						anchor = "N",
-						prompt_position = "top",
-						mirror = true,
-						width = function(_, max_columns, _)
-							return math.min(max_columns, 120)
-						end,
-						height = function(_, _, max_lines)
-							return math.min(max_lines, 15)
-						end,
-					},
-					border = true,
-					borderchars = {
-						prompt = { "─", "│", " ", "│", "╭", "╮", "│", "│" },
-						results = { "─", "│", "─", "│", "├", "┤", "╯", "╰" },
-						preview = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
-					},
+				pickers = {
+					find_files = layout,
+					git_commits = layout,
+					git_branches = layout,
 				},
 				extensions = {
 					["ui-select"] = {
-						require("telescope.themes").get_dropdown({}),
+						require("telescope.themes").get_cursor({}),
 					},
 				},
 			})
