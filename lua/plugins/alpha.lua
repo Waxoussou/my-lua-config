bc6d66ced397cbf5fe12e4d34951bee0f499cac7
maxime.abehserra <maxime.abehserra@bluemind.net>
2025-09-24 09:57:46 +0200

    Plugins: Alpha - update ascii Bluemind

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,55 +1,58 @@
 return {
-  'goolord/alpha-nvim',
-  dependencies = { 'echasnovski/mini.icons' },
-  opts = function ()
-    local dashboard = require("alpha.themes.dashboard")
+	"goolord/alpha-nvim",
+	dependencies = { "echasnovski/mini.icons" },
+	opts = function()
+		local dashboard = require("alpha.themes.dashboard")
+		local logo = [[
 
-    local logo = [[
-                                                    
-                            :::::                   
-                         :::::::::::                
-                      :::::::::::::::::             
-                   ::::::           ::::::          
-                  :::::     +++++     :::::         
-                  ::::   +++++++++++   ::::         
-                  ::::-+++++++++++++++-::::         
-                  :==++++++++   ++++++++==:         
-               :::-+++++++         +++++++-:::      
-            ::::::-++++               ++++-::::::   
-            ::::::=++++               ++++=::::::   
-            ::::: +++++               +++++ :::::   
-            ::::: +++++               +++++ :::::   
-            ::::: +++++               +++++ :::::   
-            ::::: +++++++++       +++++++++ :::::   
-            :::::  +++++++++++ +++++++++++  :::::   
-            ::::::    +++++++++++++++++    ::::::   
-             :::::::     ===+++++===     :::::::    
-                :::::::::::::-=-:::::::::::::       
-                   ::::::::::: :::::::::::          
-                      ::::         ::::             
-    
-    
-██████  ██      ██    ██ ███████ ███    ███ ██ ███    ██ ██████  
-██   ██ ██      ██    ██ ██      ████  ████ ██ ████   ██ ██   ██ 
-██████  ██      ██    ██ █████   ██ ████ ██ ██ ██ ██  ██ ██   ██ 
-██   ██ ██      ██    ██ ██      ██  ██  ██ ██ ██  ██ ██ ██   ██ 
-██████  ███████  ██████  ███████ ██      ██ ██ ██   ████ ██████  
-                                                                 
-                                                                 
+                            :::::
+                         :::::::::::
+                      :::::::::::::::::
+                   ::::::           ::::::
+                  :::::     +++++     :::::
+                  ::::   +++++++++++   ::::
+                  ::::-+++++++++++++++-::::
+                  :==++++++++   ++++++++==:
+               :::-+++++++         +++++++-:::
+            ::::::-++++               ++++-::::::
+            ::::::=++++               ++++=::::::
+            ::::: +++++               +++++ :::::
+            ::::: +++++               +++++ :::::
+            ::::: +++++               +++++ :::::
+            ::::: +++++++++       +++++++++ :::::
+            :::::  +++++++++++ +++++++++++  :::::
+            ::::::    +++++++++++++++++    ::::::
+             :::::::     ===+++++===     :::::::
+                :::::::::::::-=-:::::::::::::
+                   ::::::::::: :::::::::::
+                      ::::         ::::
+
+
+██████  ██      ██    ██ ███████ ███    ███ ██ ███    ██ ██████
+██   ██ ██      ██    ██ ██      ████  ████ ██ ████   ██ ██   ██
+██████  ██      ██    ██ █████   ██ ████ ██ ██ ██ ██  ██ ██   ██
+██   ██ ██      ██    ██ ██      ██  ██  ██ ██ ██  ██ ██ ██   ██
+██████  ███████  ██████  ███████ ██      ██ ██ ██   ████ ██████
+
+
 
     ]]
 
---    local function pick_color()
---        local colors = {"String", "Identifier", "Keyword", "Number"}
---        return colors[math.random(#colors)]
---    end
-    dashboard.section.header.val = vim.split(logo, "\n")
-    dashboard.section.header.opts.hl = "Identifier"
---    dashboard.section.header.opts = { hl = { {"AlphaHeader",13,20 }} }
-    return dashboard
-  end,
+		dashboard.section.header.val = vim.split(logo, "\n")
+		dashboard.section.header.opts.hl = "Identifier"
+		dashboard.section.buttons.val = {
+			dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+			dashboard.button("f", "󰈞  Find file", ":Telescope find_files <CR>"),
+			-- dashboard.button("p", "󰈞  Find project", ":Telescope projects <CR>"),
+			dashboard.button("q", "󰅚  Quit NVIM", ":qa<CR>"),
+		}
+		--    dashboard.section.header.opts = { hl = { {"AlphaHeader",13,20 }} }
+		dashboard.section.header.opts.hl = "GruvboxBlue"
 
-  config = function ()
-      require'alpha'.setup(require'alpha.themes.dashboard'.config)
-  end
+		return dashboard
+	end,
+
+	config = function()
+		require("alpha").setup(require("alpha.themes.dashboard").config)
+	end,
 }
