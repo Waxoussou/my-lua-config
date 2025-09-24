0767f187cbc2d4473b9401edfac5ac2f509dd9b4
maxime.abehserra <maxime.abehserra@bluemind.net>
2025-09-24 09:58:28 +0200

    Plugin: neotree - change shortcut to use leader instead of Ctrl

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,19 +1,22 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
-    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-  },
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+	},
 
-  opts = {
-    filesystem = {
-      follow_current_file = { enabled = true },
-    },
-  },
-  keys = {
-    { "<C-e>", "<Cmd>Neotree toggle<CR>", desc = "Toggle NeoTree" },
-  }
-
+	lazy = false, -- neo-tree will lazily load itself
+	---@module "neo-tree"
+	---@type neotree.Config?
+	opts = {
+		filesystem = {
+			follow_current_file = { enabled = true },
+		},
+	},
+	keys = {
+		{ "<leader>e", "<Cmd>Neotree toggle<CR>", desc = "Toggle NeoTree" },
+	},
 }
