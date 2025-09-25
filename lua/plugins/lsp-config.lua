dfff520cf580a0aea394be16c650a89df3085a80
maxime.abehserra <maxime.abehserra@bluemind.net>
2025-09-25 19:18:47 +0200

    Plugins: lsp-config - vue server path fixed

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -18,8 +18,10 @@ return {
         lazy = false,
         config = function()
             local mason_registry = require("mason-registry")
-            local vue_language_server_path = mason_registry.get_package("vue-language-server"):get_install_path()
-                .. "/node_modules/@vue/language-server"
+            -- local vue_language_server_path = mason_registry.get_package("vue-language-server"):get_install_path()
+            --     .. "/node_modules/@vue/language-server"
+
+            local vue_language_server_path = "$HOME/.local/share/nvim/mason/packages/vue-language-server/node_modules"
             local lspconfig = require("lspconfig")
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
             lspconfig.lua_ls.setup({ capabilities = capabilities })
