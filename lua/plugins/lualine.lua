f9703559774320de379ac9eb630b2d5d454605f0
maxime.abehserra <maxime.abehserra@bluemind.net>
2025-10-15 08:42:11 +0200

    [plugin] lualine: filename show relative path

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,9 +1,16 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  opts = {
-    options = {
-      theme = 'dracula'
-    },
-  },
+	"nvim-lualine/lualine.nvim",
+	opts = {
+		options = {
+			theme = "dracula",
+		},
+		sections = {
+			lualine_c = {
+				{
+					"filename",
+					path = 1, -- 0 = nom seul, 1 = chemin relatif, 2 = chemin absolu
+				},
+			},
+		},
+	},
 }
-
