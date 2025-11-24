aa75ff35d9ca66bfb7f6fcd4248d74eab2aef73e
maxime.abehserra <maxime.abehserra@bluemind.net>
2025-11-24 13:34:52 +0100

    [Plugins]: treesitter - auto install true

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,13 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local configs = require("nvim-treesitter.configs")
-		configs.setup({
-			ensure_installed = { "lua", "javascript", "typescript", "vue", "java" },
-			highlight = { enable = true },
-			--indent = { enable = false },
-		})
-	end,
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+        local configs = require("nvim-treesitter.configs")
+        configs.setup({
+            ensure_installed = { "lua", "javascript", "typescript", "vue", "java", "dart" },
+            highlight = { enable = true },
+            auto_install = true,
+            --indent = { enable = false },
+        })
+    end,
 }
